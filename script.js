a543a3ef2ec558ffc4d0e5bebdb42513fce4afbb
mbodenchak <mbodenchak@outlook.com>
2022-04-02 15:17:51 -0700

    sort array method

--- script.js
+++ script.js
@@ -39,6 +39,11 @@ function doubleMoney() {
   updateDOM();
 }
 
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
+  updateDOM();
+}
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
@@ -69,3 +74,4 @@ function formatMoney(number) {
 // Event Listeners
 addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
+sortBtn.addEventListener("click", sortByRichest);
